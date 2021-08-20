f030c72346ad9f81b067d35dfc8c9e5f2ba4155b
Hakan <33194435+croc92@users.noreply.github.com>
2021-08-20 02:35:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,54 +27,16 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.score').textContent = score;
             document.querySelector('body').style.backgroundColor = 'red';
             document.querySelector('.number').textContent = randomNumber;
-            Navigator.vibrate(100);
         }
 
     }
-
-    // // when the input is higher
-    // else if (userGuess > randomNumber) {
-
-    //     if (score > 1) {
-    //         document.querySelector('.message').textContent = 'high';
-    //         score--;
-    //         document.querySelector('.score').textContent = score;
-    //     } else {
-    //         document.querySelector('.message').textContent = 'You LOST!';
-    //         score = 0
-    //         document.querySelector('.score').textContent = score;
-    //         document.querySelector('body').style.backgroundColor = 'red';
-    //         document.querySelector('.number').textContent = randomNumber;
-
-    //     }
-
-    // }
-    // // when the input is lower
-    // else if (userGuess < randomNumber) {
-    //     if (score > 1) {
-    //         document.querySelector('.message').textContent = 'low';
-    //         score--;
-    //         document.querySelector('.score').textContent = score;
-    //     } else {
-    //         document.querySelector('.message').textContent = 'You LOST!';
-    //         score = 0
-    //         document.querySelector('.score').textContent = score;
-    //         document.querySelector('body').style.backgroundColor = 'red';
-    //         document.querySelector('.number').textContent = randomNumber;
-    //     }
-
-    // }
     // when the input is right
     else {
         document.querySelector('.message').textContent = 'Bingo! You WON!';
         document.querySelector('body').style.backgroundColor = 'green';
         document.querySelector('.number').textContent = randomNumber;
         document.querySelector('.highscore').textContent = score;
-        Navigator.vibrate(100);
     }
-
-
-
 
 });
 
